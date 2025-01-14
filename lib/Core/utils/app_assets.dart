57f6e10ac7276857dd1fe457bb0aa299643c1859
Ahmed Hamdi <ahmedhamdi725@gmail.com>
2025-01-14 19:01:13 +0200

    Add image assets and update pubspec.yaml for flutter_svg support

--- lib/Core/utils/app_assets.dart
+++ lib/Core/utils/app_assets.dart
@@ -2,5 +2,12 @@
 class AppAssets {
   AppAssets._();
   
+  static const String imagesBackground = "assets/images/background.svg";
+
+  static const String imagesGameCard1 = "assets/images/game_card_1.svg";
+
+  static const String imagesGameCard2 = "assets/images/game_card_2.svg";
+
+  static const String imagesGameCard3 = "assets/images/game_card_3.svg";
 }
 
